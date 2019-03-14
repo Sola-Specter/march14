78c32e9a862c26ba54a962b92bf4d3f17cf96b19
sola_specter <691824@student.tor.loc>
2019-03-14 11:54:05 -0400

    final work done

---
+++ mycommon.js
@@ -0,0 +1,14 @@
+$(document).ready(function()
+{
+    $("button").click(function()
+    {
+        $(this).hide();
+    });
+
+    
+    $("p").click(function()
+    {
+    alert("Hello user!!! You clicked on the alert");
+    });
+    
+});
